9eea14ae4100e674614ca075defa3a666797efb8
Andrew Smith <andrew@keansmith.com>
2022-10-12 09:09:05 -0500

    Remove CTA from Officer Career

---
+++ components/cta/CTA.js
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import Link from "next/link";
+import Body from "/components/Body";
+
+export default function CTA({ intro, url, title }) {
+  const { ref, inView } = useInView({ threshold: 0.25 });
+  const animation = useAnimation();
+  useEffect(() => {
+    if (inView) {
+      animation.start({ opacity: 1, y: 0, transition: { duration: 0.5 } });
+    } else {
+      animation.start({ opacity: 0, y: 20, transition: { duration: 0.5 } });
+    }
+  }, [inView]);
+
+  return (
+    <div ref={ref}>
+      <motion.div animate={animation} initial={{ opacity: 1, y: 0, transition: { duration: 0.5 } }}>
+        {intro && (
+          <div className="text-center">
+            <Body value={intro} />
+          </div>
+        )}
+        {title && (
+          <div className="page-ctas">
+            <Link href={url}>
+              <a className="btn-cta">{title}</a>
+            </Link>
+          </div>
+        )}
+      </motion.div>
+    </div>
+  );
+}
--- components/node--officer-career.js
+++ components/node--officer-career.js
@@ -1,42 +1,14 @@
 import { Container } from "react-bootstrap";
 import Banner from "/components/blocks/banner";
 import Paragraph from "/components/paragraphs/Paragraph";
-//import CTA from "/components/page/cta/CTA";
 
 export function NodeOfficerCareer({ node }) {
-  // Process CTAs
-  /*
-  const ctaIntro = node.field_paragraph_cta?.field_cta_intro?.processed
-    ? node.field_paragraph_cta?.field_cta_intro?.processed
-    : null;
-  let ctaTitle = null;
-  let ctaLink = null;
-
-  if (node.field_paragraph_cta?.field_cta_link) {
-    node.field_paragraph_cta.field_cta_link.map((cta) => {
-      ctaTitle = cta.title;
-      ctaLink = cta.uri.replace("internal:", "");
-    });
-  }
-  */
-
-  /* Process BG Image */
+  
   let bgImageSrc = null;
   {
     node.field_banner?.image_style_uri?.banner
       ? (bgImageSrc = `${node.field_banner.image_style_uri.banner}`)
       : (bgImageSrc = null);
-  }
-
-  const ctaIntro = node.field_paragraph_cta?.field_cta_intro?.processed
-    ? node.field_paragraph_cta?.field_cta_intro?.processed
-    : null;
-  let ctaTitle = null;
-  let ctaLink = null;
-
-  if (node.field_paragraph_cta?.field_cta_link) {
-    ctaTitle = node.field_paragraph_cta.field_cta_link.title;
-    ctaLink = node.field_paragraph_cta.field_cta_link.uri.replace("internal:", "");
   }
 
   return (
@@ -48,7 +20,6 @@ export function NodeOfficerCareer({ node }) {
             node.field_paragraphs.map((paragraph, idx) => {
               return <Paragraph content={paragraph} key={idx} />;
             })}
-          {/**{ctaTitle && <CTA intro={ctaIntro} title={ctaTitle} url={ctaLink} />}*/}
         </Container>
       </article>
     </>
