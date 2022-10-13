1d0a47ec3f3c523c21df8c0f5128446de4777383
Andrew Smith <andrew@keansmith.com>
2022-10-13 11:55:28 -0500

    Resolve build errro

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -3,7 +3,6 @@ import Actions from "./Actions";
 import Mission from "./Mission";
 import Social from "./Social";
 import Credits from "./Credits";
-import MenuData2 from "/data/MenuData2";
 
 export default function Footer({ data, menus }) {
   return (
