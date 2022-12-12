4af60a164cf99327bd8acaa271846170b62bd5ed
Andrew Smith <andrew@keansmith.com>
2022-12-12 09:12:59 -0600

    Remove Connect Actions from Footer

--- components/layout/footer/Footer.js
+++ components/layout/footer/Footer.js
@@ -1,42 +1,40 @@
 import Benefits from "./Benefits";
-import Actions from "./Actions";
 import Mission from "./Mission";
 import Social from "./Social";
 import Credits from "./Credits";
 
-export default function Footer({ 
-  facebook, 
-  instagram, 
-  youtube, 
-  benefitsTitle, 
-  benefitsBody, 
-  benefitsLink, 
-  actionsBody, 
-  menus, 
+export default function Footer({
+  facebook,
+  instagram,
+  youtube,
+  benefitsTitle,
+  benefitsBody,
+  benefitsLink,
+
+  menus,
   author,
-  mission 
+  mission
 }) {
   return (
     <footer>
-      <Benefits 
-        title={benefitsTitle} 
-        body={benefitsBody} 
-        link={benefitsLink.uri} 
-        linkText={benefitsLink.title} 
+      <Benefits
+        title={benefitsTitle}
+        body={benefitsBody}
+        link={benefitsLink.uri}
+        linkText={benefitsLink.title}
       />
-      <Actions body={actionsBody} />
-      <Mission 
-        mission={mission} 
+      <Mission
+        mission={mission}
       />
       <Social
-        facebook={facebook.uri} 
-        instagram={instagram.uri} 
-        youtube={youtube.uri} 
-        menu1={menus.footer1.tree} 
-        menu2={menus.footer2.tree}  
+        facebook={facebook.uri}
+        instagram={instagram.uri}
+        youtube={youtube.uri}
+        menu1={menus.footer1.tree}
+        menu2={menus.footer2.tree}
       />
-      <Credits 
-        author={author} 
+      <Credits
+        author={author}
       />
     </footer>
   );
