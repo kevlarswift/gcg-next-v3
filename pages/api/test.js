13f7dd39baa12c202695f2806364471e5088724d
Andrew Smith <andrew@keansmith.com>
2022-12-15 12:10:25 -0600

    Working example of Submitting to Drupal webform via server api route

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,17 +1,18 @@
 import { drupal } from "/lib/drupal"
 
-export default async function handler(request,response) {
+export default async function handler(request, response) {
+  
   try {
     if (request.method === "POST") {
+      const body = JSON.parse(request.body)
       const url = drupal.buildUrl("/webform_rest/submit")
-
+      
       // Submit to Drupal.
       const result = await drupal.fetch(url.toString(), {
         method: "POST",
         body: JSON.stringify({
-          webform_id: "contact",
-          name: request.body.name,
-          email: request.body.email,
+          webform_id: "test",
+          ...body
         }),
         headers: {
           "Content-Type": "application/json",
--- pages/test.js
+++ pages/test.js
@@ -1,8 +1,13 @@
-export default function TestPage() {
+import Head from "next/head";
+import { drupal } from "/lib/drupal";
+import { Container } from "react-bootstrap";
+import { Layout } from "/components/layout";
+
+export default function TestPage({ menus, global }) {
   async function handleSubmit(event) {
     event.preventDefault()
-
-    const response = await fetch(`/api/contact`, {
+    
+    const response = await fetch(`/api/test`, {
       method: "POST",
       body: JSON.stringify({
         name: event.target.name.value,
@@ -11,7 +16,7 @@ export default function TestPage() {
     })
 
     if (response.ok) {
-      console.log('ok')
+      console.log("ok")
     }
 
     // Handle error.
@@ -19,19 +24,45 @@ export default function TestPage() {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <p>Implement your form here</p>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  )
+    <>
+      <Head>
+        <title>Test Form | GoCoastGuard.com</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </Head>
+      <Layout menus={menus} global={global}>
+        <Container>
+          <div>
+          <form onSubmit={handleSubmit}>
+            <div>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" />
+            </div>
+            <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" />
+            </div>
+            <div>
+            <button type="submit">Submit</button>
+            </div>
+          </form>
+          </div>
+        </Container>
+      </Layout>
+    </>
+  );
+}
+
+export async function getStaticProps(context) {
+  
+  return {
+    props: {
+      menus: {
+        main: await drupal.getMenu("main"),
+        footer1: await drupal.getMenu("footer"),
+        footer2: await drupal.getMenu("footer-menu-2")
+      },
+      global: await drupal.getResource("node--global", "132de760-f931-4656-a5a9-9a13455d232f"),
+    },
+    revalidate: 60,
+  };
 }
