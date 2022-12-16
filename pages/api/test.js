e93aeddfb8feef47eaabf0390770df0df3162a23
Andrew Smith <andrew@keansmith.com>
2022-12-16 08:06:40 -0600

    Test PQ con't

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,12 +1,10 @@
 import { drupal } from "/lib/drupal"
 
-export default async function handler( request, response ) {
-  
-  try {
+export default async function handler( request, response ) { 
+  try {   
     if (request.method === "POST") {
       const body = JSON.parse(request.body)
       const url = drupal.buildUrl("/webform_rest/submit")
-      
       // Submit to Drupal.
       const result = await drupal.fetch(url.toString(), {
         method: "POST",
@@ -18,11 +16,9 @@ export default async function handler( request, response ) {
           "Content-Type": "application/json",
         },
       })
-
       if (!result.ok) {
         throw new Error()
       }
-
       response.status(200).end()
     }
   } catch (error) {
